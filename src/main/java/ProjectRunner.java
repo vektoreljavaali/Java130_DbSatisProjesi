54841f51a7e09770d7288441c30a3067d885754f
Muhammet Ali HACA <vektoreljavaali@gmail.com>
2022-03-20 12:46:18 +0300

    Tüm CRUD işlemleri yapıldı.

--- src/main/java/ProjectRunner.java
+++ src/main/java/ProjectRunner.java
@@ -1,8 +1,20 @@
 import controller.MusteriController;
+import repository.entity.Musteri;
+import services.MusteriService;
 
 public class ProjectRunner {
     public static void main(String[] args) {
         MusteriController musteriController = new MusteriController();
-        musteriController.MusteriKaydet();
+        //musteriController.MusteriKaydet();
+        MusteriService ms = new MusteriService();
+        for(Musteri musteri:  ms.findAll()){
+            System.out.println("müşteri id.....: "+ musteri.getId());
+            System.out.println("müşteri ad.....: "+ musteri.getAd());
+            System.out.println("müşteri soyad..: "+ musteri.getSoyad());
+            System.out.println("--------------------------------");
+        }
+
+
+
     }
 }
--- src/main/java/repository/MusteriRepository.java
+++ src/main/java/repository/MusteriRepository.java
@@ -4,6 +4,8 @@ import repository.entity.Musteri;
 import utility.ConnectionDb;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MusteriRepository {
@@ -63,9 +65,42 @@ public class MusteriRepository {
     }
 
     public void delete(long id){
+        String SQL = "delete from tblmusteri where id = "+id;
+        try{
+            pst = Db.getConnection().prepareCall(SQL);
+            pst.executeUpdate();
+            Db.closeConnection();
+        }catch (Exception e){
+            System.out.println("Müşteri delete error...: "+e.toString());
+        }
     }
 
     public List<Musteri> findAll(){
-        return null;
+        List<Musteri> list = new ArrayList<>();
+        String SQL = "select * from tblmusteri";
+        try{
+            pst = Db.getConnection().prepareCall(SQL);
+            /**
+             * Select sonucunda dönen tablo bize ResultSet olarak getirilir.
+             */
+            ResultSet rs = pst.executeQuery();
+            while(rs.next()){
+                String ad = rs.getString("ad");
+                String soyad = rs.getString("soyad");
+                String adres = rs.getString("adres");
+                String telefon = rs.getString("telefon");
+                Musteri mst = new Musteri();
+                mst.setId(rs.getLong("id"));
+                mst.setAd(ad);
+                mst.setSoyad(rs.getString("soyad"));
+                mst.setTelefon(telefon);
+                mst.setAdres(adres);
+                list.add(mst);
+            }
+            Db.closeConnection();
+        }catch (Exception e){
+            System.out.println("Müşteri List Error..: "+e.toString());
+        }
+        return list;
     }
 }
